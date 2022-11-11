e60bea13d89eb3e8d02bffbef7e8c1e15461c8d4
italo <italogithub@gmail.com>
2022-11-11 08:56:13 -0300

    update

--- main.rs
+++ main.rs
@@ -4,6 +4,15 @@ extern crate getopts;
 
 use chess:: {board, MoveGen, Color, BoardStatus, ChessMove, ALL RANKS, Piece, get_rank }
 use std::env;
-use std::io::{self, BufRead};
+use std::io::{ self, BufRead };
 use std::str::FromStr;
-use std
+use std::time::{ Duration, Instant};
+use args::{ Args, ArgsError };
+use getopts::Occur;
+use colorred::{ ColoredString, Colorize };
+mod piece_values;
+mod benchmarks;
+
+fn main(){
+    println!("Hello world");
+}
